7014f3a02b4a179f692b63470e5d11abadc4fdcc
LuizAmancio <luizgagnoamancio@gmail.com>
2022-05-09 10:48:41 -0300

    Api rest teste finalizada

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
@@ -19,18 +20,61 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send(`The course with the id of ${req.params.id} does not exist`)//404
+    if (!course) return res.status(404).send(`The course with the id of ${req.params.id} does not exist`)//404
     res.send(course)
 })
 
 app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body)
+    
+    if (error) return res.status(400).send(error.details[0].message)
+
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
     courses.push(course)
+    res.send(course)
 })
 
+app.put('/api/courses/:id', (req, res) => {
+    //Look up the course
+    //If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send(`The course with the id of ${req.params.id} does not exist`)//404
+
+    //Validate
+    //If invalid, return 400 - Bad request
+    const { error } = validateCourse(req.body)
+    
+    if (error) return res.status(400).send(error.details[0].message)
+
+    //Update course
+    course.name = req.body.name
+    //Return the updated course
+    res.send(course)
+})
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send(`The course with the id of ${req.params.id} does not exist`)//404
+    
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+
+    res.send(courses)
+
+})
+
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    return schema.validate(course)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
