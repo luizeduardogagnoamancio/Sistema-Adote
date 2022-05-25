ca2834c535dcfe6f606cba04639813706362a5d3
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2022-05-25 11:07:14 -0300

    Add endereço no .env

--- backendTrue/server.js
+++ backendTrue/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.MONGO_URI)
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
